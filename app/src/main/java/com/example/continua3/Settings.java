2d741293383798578c513bf4c92b2c892f6b4435
ManuelCuri <git config --global user.email>
2022-01-10 16:32:27 -0500

    implements of Camera in Setting

--- app/src/main/java/com/example/continua3/Settings.java
+++ app/src/main/java/com/example/continua3/Settings.java
@@ -1,19 +1,32 @@
 package com.example.continua3;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import com.bumptech.glide.Glide;
 
-public class Settings extends Fragment{
+public class Settings extends AppCompatActivity {
 
     ImageView set;
     ProgressDialog pd;
@@ -29,10 +42,148 @@ public class Settings extends Fragment{
     AppCompatButton editImage;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle SavedInstanceState){
-        View view = inflater.inflate(R.layout.settings, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+        editImage = findViewById(R.id.edit_image);
+        set = findViewById(R.id.profile_image);
+        pd = new ProgressDialog(this);
+        pd.setCanceledOnTouchOutside(false);
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
-        return view;
+        editImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pd.setMessage("Updating Profile Picture");
+                profileOrCoverImage = "image";
+                showImagePicDialog();
+            }
+        });
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Glide.with(Settings.this).load(imageUri).into(set);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == Activity.RESULT_OK) {
+            if (requestCode == IMAGE_PICK_GALLERY_REQUEST) {
+                imageUri = data.getData();
+                uploadProfileCoverPhoto(imageUri);
+            }
+            if (requestCode == IMAGE_PICK_CAMERA_REQUEST) {
+                uploadProfileCoverPhoto(imageUri);
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CAMERA_REQUEST: {
+                if (grantResults.length > 0) {
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted && writeStorageAccepted) {
+                        pickFromCamera();
+                    } else {
+                        Toast.makeText(this, "Please enable camera and storage permission", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Something went wrong! try again...", Toast.LENGTH_LONG).show();
+                }
+            }
+            break;
+            case STORAGE_REQUEST: {
+                if (grantResults.length > 0) {
+                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (writeStorageAccepted) {
+                        pickFromGallery();
+                    } else {
+                        Toast.makeText(this, "Please enable storage permission", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Something went wrong! try again...", Toast.LENGTH_LONG).show();
+                }
+            }
+            break;
+        }
+    }
+
+
+    private void showImagePicDialog() {
+        String options[] = {"Camera", "Gallery"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick image from");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0) {
+                    if (!checkCameraPermission()) {
+                        requestCameraPermission();
+                    } else {
+                        pickFromCamera();
+                    }
+                } else if (which == 1) {
+                    if (!checkStoragePermission()) {
+                        requestStoragePermission();
+                    } else {
+                        pickFromGallery();
+                    }
+                }
+            }
+        });
+        builder.create().show();
+    }
+
+    private Boolean checkCameraPermission() {
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+
+        return result && result1;
+    }
+
+    private Boolean checkStoragePermission() {
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void requestCameraPermission() {
+        requestPermissions(cameraPermission, CAMERA_REQUEST);
+    }
+
+    private void requestStoragePermission() {
+        requestPermissions(storagePermission, STORAGE_REQUEST);
+    }
+
+    private void pickFromCamera() {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE, "Temp_pic");
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Temp description");
+        imageUri = this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(cameraIntent, IMAGE_PICK_CAMERA_REQUEST);
+    }
+
+    private void pickFromGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, IMAGE_PICK_GALLERY_REQUEST);
+    }
+
+    private void uploadProfileCoverPhoto(final Uri uri) {
+        pd.show();
+        // function to save in to database {}
+        pd.dismiss();
+        Toast.makeText(Settings.this, "Updated Photo", Toast.LENGTH_LONG).show();
+    }
 }
